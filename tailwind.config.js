b00f14f7a6e9b50603d0af3b3abc377718f5d2ee
unknown <deividcuellocuevas@gmail.com>
2023-07-10 15:06:55 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    colors:{
+      primary: '#0071DC',
+      white: '#FFFFFF',
+      yellow: '#FFC220',
+      black: '#000000',
+      gray: '#777777',
+      green: '#2A8703'
+    },
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
